8cd5805f1fd2555787ae71fbce64a16537b6ca83
Shendy <sanghokage@gmail.com>
2020-04-07 15:29:08 +0700

    Initial Commit

---
+++ Zensei/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Zensei
+//
+//  Created by Shendy Aditya Syamsudin on 07/04/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
