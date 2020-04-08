d9a8374ec78cbfa210726b67ee7fb1db0c85aae0
Shendy <sanghokage@gmail.com>
2020-04-08 21:46:00 +0700

    View Main Menu

--- Zensei/ViewController.swift
+++ Zensei/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        if indexPath.row == 0 {
+            return  cell
+            
+        }else if indexPath.row == 1 {
+            return  cell
+            
+        }else{
+            return cell
+        }
+    }
+    
+    
+    
 }
 
